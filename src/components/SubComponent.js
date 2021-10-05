d943fac2e83ed3e1f85a72920ec8b3c32a5893e9
Dinesh Mandloi <dineshmandloi19981@gmail.com>
2021-10-05 10:08:53 +0530

    Initial commit

---
+++ src/components/SubComponent.js
@@ -0,0 +1,6 @@
+//import './SubComponent.css';
+
+function SubComponent(){
+    return <p>i am sub components</p>;
+}
+export default SubComponent;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import ReactDom from "react-dom";
+import './index.css';
+import App from './App.js';
+
+ReactDom.render(
+<App/>,
+document.getElementById("root")
+);
